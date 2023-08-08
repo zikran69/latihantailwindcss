81134c9fae0b9a8f219da7628e9c8989e6a6ad4f
zikran69 <zikzigi69@gmail.com>
2023-08-08 17:59:59 +0700

    menambahkan konten about

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
         primary: '#facc15',
         dark: '#18181b',
+      },
+      screens: {
+        '2xl': '1320px',
       }
     },
   },
